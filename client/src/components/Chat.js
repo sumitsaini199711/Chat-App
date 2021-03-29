779284bd269c884d2106c2e04202c9b65be778a3
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-29 21:39:09 +0530

    Adding react router for Join and Chat Component

---
+++ client/src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Chat() {
+  return <div>Chat Component</div>;
+}
+
+export default Chat;
---
+++ client/src/components/Join.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Join() {
+  return <div>Join Component</div>;
+}
+
+export default Join;
