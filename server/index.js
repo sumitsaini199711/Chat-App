2ea62e059b09d4ba36ba9fb2bf2b0911528bbed4
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-29 22:04:18 +0530

    Setting up express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
