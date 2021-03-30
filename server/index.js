8bf33ab8a8ce28e69e383053af71200b4d370ede
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-30 11:37:41 +0530

    Setting socket message receive at server Side

--- server/index.js
+++ server/index.js
@@ -9,14 +9,39 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 app.use(router);
 
 //Socket connection
 io.on("connection", (socket) => {
-  console.log("We have a new connection!!!");
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, Welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
