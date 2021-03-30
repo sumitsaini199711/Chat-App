926a879d5e7aec35405561c446da72ae8cc4d83b
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-30 12:45:48 +0530

    Adding chatbox messages component

--- server/index.js
+++ server/index.js
@@ -32,6 +32,10 @@ io.on("connection", (socket) => {
       .emit("message", { user: "admin", text: `${user.name}, has joined!` });
 
     socket.join(user.room);
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
@@ -45,7 +49,18 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User had left !!!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
 
